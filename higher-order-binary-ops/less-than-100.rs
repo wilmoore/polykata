97dbae17f1823db1441331f927e523daaec694a3
Wil Moore III <wil.moore@wilmoore.com>
2013-10-13 16:13:29 -0600

    added rust implementation

--- higher-order-binary-ops/less-than-100.rs
+++ higher-order-binary-ops/less-than-100.rs
@@ -1,1 +1,1 @@
--
+fn main() {  let lt  = |ro: int| { |lo: int| lo < ro; };  let num = [5, 500, 50, 101, 200, 35];  let out = num.iter().filter(lt(100));  println!("less than 100: {}", out);  /* => less than 100: [5 50 35] */}
