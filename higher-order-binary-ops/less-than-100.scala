205fba89bb868f5de24604c7e8abbf454575c34a
Wil Moore III <wil.moore@wilmoore.com>
2015-03-10 17:40:40 -0600

    add a note about a better scala syntax

--- higher-order-binary-ops/less-than-100.scala
+++ higher-order-binary-ops/less-than-100.scala
@@ -1,7 +1,7 @@
 def lt(ro: Int)(lo: Int): Boolean = lo < ro
 
 val num = List(5, 500, 50, 101, 200, 35)
-val out = num filter { lt(100) }
+val out = num filter { lt(100) } // or: num filter { _ < 100 } since the < operator is a curried function.
 
 println(s"less than 100: [ ${out.mkString(", ")} ]")
 //=> less than 100: [ 5, 50, 35 ]
